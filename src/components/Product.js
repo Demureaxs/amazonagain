9664b774e3a7c28b1213d0c03a8bd7e61aed94c8
demureaxs <118724010+Demureaxs@users.noreply.github.com>
2023-01-30 14:37:20 +0700

    third times a charm

---
+++ src/components/Product.js
@@ -0,0 +1,48 @@
+import React, { use, useState } from 'react';
+import Image from 'next/image';
+import { StarIcon } from '@heroicons/react/solid';
+
+function Product({ id, title, price, description, category, image }) {
+  const [rating] = useState(Math.floor(Math.random() * 4) + 2);
+
+  const [hasPrime] = useState(Math.random() < 0.5);
+
+  return (
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+        {category}
+      </p>
+      <Image
+        className=" h-[200px] w-[200px] object-contain mx-auto"
+        src={image}
+        height={200}
+        width={200}
+      />
+
+      <h4 className="my-3">{title}</h4>
+
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => {
+            return <StarIcon key={i} className="h-5 text-yellow-600" />;
+          })}
+      </div>
+
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
+
+      <div className="mt-auto mb-5">{`$${price}`}</div>
+
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-5">
+          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+          <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+        </div>
+      )}
+
+      <button className="mt-auto button">Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
---
+++ src/pages/index.js
@@ -0,0 +1,38 @@
+import Header from '@/components/Header';
+import Head from 'next/head';
+import Banner from '@/components/Banner';
+import ProductFeed from '@/components/ProductFeed';
+
+export default function Home(props) {
+  return (
+    <div className="bg-gray-100">
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+        {/* Poduct feed */}
+        <ProductFeed products={props.products} />
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch('https://fakestoreapi.com/products').then(res =>
+    res.json()
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
+
+//https://fakestoreapi.com/products
